fa7b18b3ead5c6bd71ffee14d8c5a34be0fe23cf
r3nzph <abarracosorenznino@gmail.com>
2024-08-26 06:21:32 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-KJ5-user
-add_lunch_combo omni_TECNO-KJ5-userdebug
-add_lunch_combo omni_TECNO-KJ5-eng
+add_lunch_combo twrp_KJ5-user
+add_lunch_combo twrp_KJ5-userdebug
+add_lunch_combo twrp_KJ5-eng
